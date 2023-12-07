5f8deb048b9ddd326b59e1c3f163df187b41b269
Laura Streit <laurajstreit@gmail.com>
2023-12-07 01:00:16 -0800

    Add transitions to footer, gallery, header things

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -12,15 +12,15 @@ function Nav () {
     <nav class="text-white w-full flex h-20 px-10 bg-lilipurple sticky top-0 drop-shadow-sm items-center gap-x-6">
       <div className="text-4xl font-black block">
         <a href="/">
-        <img class="w-11 opacity-100 hover:transition-opacity hover:opacity-70 ease-in-out duration-200" src="/src/assets/lsLogo.svg"></img>
+        <img class="w-11 opacity-100 hover:transition-opacity hover:opacity-70 ease-in-out duration-500" src="/src/assets/lsLogo.svg"></img>
         </a>
       </div>
       <FlexContainer></FlexContainer>
       <div className="flex flex-row-reverse">
-        <ul className="flex gap-x-6 text-white text-sm">
-              <li className='opacity-100 hover:transition-opacity hover:opacity-70 ease-in-out duration-200'>About</li>
-              <li className='opacity-100 hover:transition-opacity hover:opacity-70 ease-in-out duration-200'>Work</li>
-              <li className='opacity-100 hover:transition-opacity hover:opacity-70 ease-in-out duration-200'>Contact</li>
+        <ul className="flex gap-x-6 text-white text-sm opacity-100">
+              <li className='opacity-100 transition-opacity hover:opacity-70 ease-in-out duration-500'>About</li>
+              <li className='opacity-100 transition-opacity hover:opacity-70 ease-in-out duration-500'>Work</li>
+              <li className='opacity-100 transition-opacity hover:opacity-70 ease-in-out duration-500'>Contact</li>
           </ul>
       </div>
     </nav>
@@ -49,13 +49,13 @@ function IntroductionSection () {
 function GalleryColumns () {
   return (
     <div className="w-full flex flex-row flex-wrap justify-center">
-      <div className="m-2"><img class="max-w-2xl rounded-xl" src="/src/assets/galleryImages/mapleMemories.png"></img></div>
-      <div className="m-2"><img class="max-w-2xl rounded-xl" src="/src/assets/galleryImages/msSocialStrategy.png"></img></div>
-      <div className="m-2"><img class="max-w-2xl rounded-xl" src="/src/assets/galleryImages/kdSocialStrategy.png"></img></div>
-      <div className="m-2"><img class="max-w-2xl h-full rounded-xl object-cover" src="/src/assets/galleryImages/mute your mic behance_cover.png"></img></div>
-      <div className="m-2"><img class="max-w-2xl rounded-xl object-cover" src="/src/assets/galleryImages/theneutral_1400x1400.png"></img></div>
-      <div className="m-2"><img class="max-w-2xl h-full rounded-xl object-cover" src="/src/assets/galleryImages/htc deck cover image.png"></img></div>
-      <div className="m-2"><img class="max-w-2xl rounded-xl object-cover" src="/src/assets/galleryImages/womenofesports_cover.png"></img></div>
+      <div className="m-2"><img class="max-w-2xl rounded-xl opacity-100 hover:transition-color hover:grayscale hover:opacity-70 ease-in-out duration-1000" src="/src/assets/galleryImages/mapleMemories.png"></img></div>
+      <div className="m-2"><img class="max-w-2xl rounded-xl opacity-100 hover:transition-color hover:grayscale hover:opacity-70 ease-in-out duration-1000" src="/src/assets/galleryImages/msSocialStrategy.png"></img></div>
+      <div className="m-2"><img class="max-w-2xl rounded-xl opacity-100 hover:transition-color hover:grayscale hover:opacity-70 ease-in-out duration-1000" src="/src/assets/galleryImages/kdSocialStrategy.png"></img></div>
+      <div className="m-2"><img class="max-w-2xl h-full rounded-xl object-cover opacity-100 hover:transition-color hover:grayscale hover:opacity-70 ease-in-out duration-1000" src="/src/assets/galleryImages/mute your mic behance_cover.png"></img></div>
+      <div className="m-2"><img class="max-w-2xl rounded-xl object-cover opacity-100 hover:transition-color hover:grayscale hover:opacity-70 ease-in-out duration-1000" src="/src/assets/galleryImages/theneutral_1400x1400.png"></img></div>
+      <div className="m-2"><img class="max-w-2xl h-full rounded-xl object-cover opacity-100 hover:transition-color hover:grayscale hover:opacity-70 ease-in-out duration-1000" src="/src/assets/galleryImages/htc deck cover image.png"></img></div>
+      <div className="m-2"><img class="max-w-2xl rounded-xl object-cover opacity-100 hover:transition-color hover:grayscale hover:opacity-70 ease-in-out duration-1000" src="/src/assets/galleryImages/womenofesports_cover.png"></img></div>
     </div>
   )
 }
@@ -67,9 +67,9 @@ function Footer () {
         © Copyright 2023 by Laura Streit
       </div>
       <div className="flex flex-row gap-2">
-        <TwitterIcon className='opacity-20 hover:transition-opacity hover:opacity-100 ease-in-out duration-200' />
-        <LinkedInIcon className='opacity-20 hover:transition-opacity hover:opacity-100 ease-in-out duration-200' />
-        <MailIcon className='opacity-20 hover:transition-opacity hover:opacity-100 ease-in-out duration-200' />
+        <div className="opacity-20 transition-opacity hover:opacity-100 ease-in-out duration-500"><TwitterIcon className=''/></div>
+        <div className="opacity-20 transition-opacity hover:opacity-100 ease-in-out duration-500"><LinkedInIcon className=''/></div>
+        <div className="opacity-20 transition-opacity hover:opacity-100 ease-in-out duration-500"><MailIcon className='' /></div>
       </div>
     </div>
   )
